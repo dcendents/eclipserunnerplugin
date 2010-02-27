c42e9cf8c20c864dbce3a6aa029ebc3334995cfa
vachacz@gmail.com <vachacz@gmail.com@eab48e92-1b34-11df-bebe-dd3048898085>
2010-02-27 00:04:44 +0000

    Java editor actions

---
+++ src/com/eclipserunner/ui/menus/SaveAsRunJavaEditorAction.java
@@ -0,0 +1,31 @@
+package com.eclipserunner.ui.menus;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+
+/**
+ * @author vachacz
+ */
+public class SaveAsRunJavaEditorAction extends Action implements IEditorActionDelegate {
+
+	private IEditorPart editorPart;
+	
+	public void setActiveEditor(IAction action, IEditorPart editorPart) {
+		this.editorPart = editorPart;
+	}
+	
+	public void run(IAction arg0) {
+		// TODO LWA dummy
+		MessageDialog.openInformation(
+			editorPart.getEditorSite().getShell(),
+			"Eclipse Runner Action", "add run configuration to bookmarks");
+	}
+
+	public void selectionChanged(IAction arg0, ISelection arg1) {
+	}
+
+}
