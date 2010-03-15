2aeea678aa22d5180c57616aca4acb8c89460626
bartosz.roznowski@gmail.com <bartosz.roznowski@gmail.com@eab48e92-1b34-11df-bebe-dd3048898085>
2010-03-15 15:37:30 +0000

    Code cleanup.

--- src/com/eclipserunner/views/actions/ToggleBookmarkModeAction.java
+++ src/com/eclipserunner/views/actions/ToggleBookmarkModeAction.java
@@ -3,6 +3,11 @@ package com.eclipserunner.views.actions;
 import com.eclipserunner.RunnerPluginPrererenceConstants;
 import com.eclipserunner.views.IRunnerView;
 
+/**
+ * Switch bookmark mode. 
+ * 
+ * @author bary
+ */
 public class ToggleBookmarkModeAction extends AbstractToggleAction {
 
 	private final IRunnerView runnerView;
@@ -11,23 +16,16 @@ public class ToggleBookmarkModeAction extends AbstractToggleAction {
 		this.runnerView = runnerView;
 
 		boolean checked = getPreferenceStore().getBoolean(RunnerPluginPrererenceConstants.BOOKMARK_FILTER_ENABLE);
-		valueChanged(checked, false);
+		setChecked(checked);
 	}
 
 	@Override
 	public void run() {
-		valueChanged(isChecked(), true);
+		getPreferenceStore().setValue(RunnerPluginPrererenceConstants.BOOKMARK_FILTER_ENABLE, isChecked());
 
 		// TODO BARY launchers filtering
 		// runnerView.
 		runnerView.refresh();
-	}
-
-	private void valueChanged(final boolean checked, boolean storeProperty) {
-		setChecked(checked);
-		if (storeProperty) {
-			getPreferenceStore().setValue(RunnerPluginPrererenceConstants.BOOKMARK_FILTER_ENABLE, checked);
-		}
 	}
 
 }
